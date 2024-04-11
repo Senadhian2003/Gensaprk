dd68c2b2891c67f53c2e88c51e036c591dcb504e
Senadhian G <senadhian@gmail.com>
2024-04-12 00:38:17 +0530

    Task 5 Completed

---
+++ Day3/AssignmentSolution/Assignment1/Task5.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Task5
+    {
+
+        static bool getName(string input, out string name) {
+            name = "";
+            string pattern = @"^[a-zA-Z\s]+$";
+
+            bool patternMatch = Regex.IsMatch(input, pattern);
+
+            if(patternMatch) { 
+                name = input;
+                return true;
+            }
+            return false;
+
+
+        }
+
+        static bool getPassword(string input, out string pass)
+        {
+            pass = "";
+            if(input.Trim().Length == 0) { 
+                return false;
+            }
+            pass = input.Trim();
+            return true;
+
+        }
+
+        static bool validate(string name, string password, out string message)
+        { message = "The user name and password do not match. Try again";
+            if(name == "ABC" &&  password == "123")
+            {
+                message = "User has been authenticated successfully";
+                return true;
+            }
+            return false;
+        }
+
+        static void PrintInfo( string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        static void authenticateUser()
+        {
+            string name;
+            string password;
+            int cnt = 0;
+            string message;
+            Console.WriteLine("Enter User name");
+            while (cnt < 3)
+            {
+                while(!getName(Console.ReadLine(), out name))
+                {
+                    Console.WriteLine("Invalid User Name. Type Correct User name");
+                }
+                Console.WriteLine("Enter Password");
+                //password = Console.ReadLine().Trim();
+
+                while(!getPassword(Console.ReadLine(), out password))
+                {
+                    Console.WriteLine("Password is Empty. Type a password");
+                }
+
+                if (!validate(name, password,out message)) {
+                    PrintInfo(message);
+                    cnt++;
+                }
+                else
+                {
+                    PrintInfo(message);
+                    break;
+                }
+
+
+            }
+            if(cnt>=3)
+            {
+                message = "The trial exceeded 3 attempts. Try again after some time";
+                PrintInfo(message);
+            }
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            authenticateUser();
+
+        }
+    }
+}
--- Day3/AssignmentSolution/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ Day3/AssignmentSolution/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75ab16863c8b3cd2d28ec9db3b4bd02a6491cff7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03a25f6bd6f438c5cc8f86b38e9d807c9dc4eb17")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
