03a25f6bd6f438c5cc8f86b38e9d807c9dc4eb17
Senadhian G <senadhian@gmail.com>
2024-04-11 23:57:03 +0530

    Task 4 completed

--- Day3/AssignmentSolution/Assignment1/Task4.cs
+++ Day3/AssignmentSolution/Assignment1/Task4.cs
@@ -2,15 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Assignment1
 {
     internal class Task4
     {
+        static bool getName(string input, out string name)
+        {
+            name = "";
+            string pattern =  @"^[a-zA-Z\s]+$";
+
+            bool patternMatch = Regex.IsMatch(input, pattern);
+
+            if (patternMatch)
+            {
+                name = input;
+                return true;
+            }
+            return false;
+
+
+        }
+        static string trimmer(string name)
+        {
+            return name.Trim();
+        }
+        static int findLength(string name)
+        {   
+            return trimmer(name).Length;
+        }
+
+        static void PrintInfo(int len)
+        {
+            if(len == 0)
+            {
+                Console.WriteLine("The User Name is Empty, The length is 0");
+            }
+            else
+            {
+                Console.WriteLine($"The length of user Name is {len}");
+            }
+        }
+
+         static void getStringLength()
+        {
+            string name;
+            Console.WriteLine("Enter User name");
+            while(!getName(Console.ReadLine(), out name))
+            {
+                Console.WriteLine("The User name is inappropriate. Type Correct User name");
+            }
+
+            PrintInfo(findLength(name));
+
+        }
 
         static void Main(string[] args)
         {
+            getStringLength();
 
         }
 
--- Day3/AssignmentSolution/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ Day3/AssignmentSolution/Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd54e5fb318470d6d9623da9fdba48c10bf87660")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75ab16863c8b3cd2d28ec9db3b4bd02a6491cff7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
