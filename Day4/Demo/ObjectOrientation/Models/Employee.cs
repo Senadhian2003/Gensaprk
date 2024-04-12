5c07998c8d183bfdb0b216078eab3e5b30a354aa
Senadhian G <senadhian@gmail.com>
2024-04-12 15:11:55 +0530

    Day 4 Object Oriented tutorial

---
+++ Day4/Demo/ObjectOrientation/Models/Employee.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientation.Models
+{
+    class Employee
+    {
+        //private int id;
+        //private string name;
+
+        //public int getId()
+        //{
+        //    return id;
+        //}
+
+        //public string getName()
+        //{
+        //    return name;
+        //}
+
+        //public void setId(int id)
+        //{
+        //    this.id = id;
+        //}
+
+        //public void setName(string name)
+        //{
+        //        this.name = name;
+        //}
+
+
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public void manipulateName()
+        {
+            Name = "Sena";
+        }
+
+
+        private double salary;
+
+        public Employee(int id)
+        {
+            Id = id;
+
+        }
+
+        public Employee(int id, string name, string email) : this(id)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public double Salary
+        {
+            get
+            {
+                return salary - salary * 10 / 100;
+            }
+
+            set
+            {
+                salary = value;
+            }
+
+
+        }
+
+        /// <summary>
+        /// Print Hours worked
+        /// </summary>
+        /// <param name="hours">No of hours</param>
+        /// <returns>String message of hours worked</returns>
+        public string Work(int hours)
+        {
+            return $"Doing work for {hours} hours from morning";
+        }
+
+
+        public void printInfo()
+        {
+            Console.WriteLine("Id\t:\t" + Id);
+            Console.WriteLine("Name\t:\t" + Name);
+            Console.WriteLine("Email\t:\t" + Email);
+
+        }
+
+
+
+    }
+}
---
+++ Day4/Demo/ObjectOrientation/Program.cs
@@ -0,0 +1,63 @@
+﻿using ObjectOrientation.Models;
+
+namespace ObjectOrientation
+{
+    internal class Program
+    {
+
+        Employee getEmployeeDetailsFromConsole(int id)
+        {
+            Employee employee = new Employee(100 + id);
+            Console.WriteLine("Enter name");
+            employee.Name = Console.ReadLine();
+            Console.WriteLine("Enter email");
+            employee.Email = Console.ReadLine();
+            return employee;
+            
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Employee employee = new Employee(101,"Sena","senadhian@gmail.com");
+            employee.Salary = 1000;
+           
+            Console.WriteLine(employee.Salary);
+
+            ////int[] ages = new int[] { 7, 8 };
+            //int[] ages = new int[2];
+            //ages[0]= 7;
+            ////ages[1]= 8;
+
+
+            //int len = ages.Length;
+            //for(int i = 0; i < len; i++)
+            //{
+            //    Console.WriteLine(ages[i]);
+            //}
+
+            Employee[] employees = new Employee[2];
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+
+                employees[i] = program.getEmployeeDetailsFromConsole(i);
+
+
+            }
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+
+                employees[i].printInfo();
+
+
+            }
+
+
+
+        }
+    }
+}
