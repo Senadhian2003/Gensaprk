f69b0264bdcb41a3c5292e5f8ee402fb02f055b2
Senadhian G <senadhian@gmail.com>
2024-06-14 02:24:32 +0530

    Rent and Fine Dashboard completed

--- 11June2024/Assignment/frontend/Assets/javascript/sidebar.js
+++ 11June2024/Assignment/frontend/Assets/javascript/sidebar.js
@@ -11,30 +11,30 @@ hamburger.addEventListener("click",function(){
 
 const sidebarLinks = document.querySelectorAll('.sidebar-link');
 
-sidebarLinks.forEach(link => {
-    link.addEventListener('click', function(e) {
-      e.preventDefault(); // Prevent the default link behavior
+// sidebarLinks.forEach(link => {
+//     link.addEventListener('click', function(e) {
+//       e.preventDefault(); // Prevent the default link behavior
   
-      // Remove the 'active' class from all links
-      sidebarLinks.forEach(link => link.classList.remove('active'));
+//       // Remove the 'active' class from all links
+//       sidebarLinks.forEach(link => link.classList.remove('active'));
   
-      // Add the 'active' class to the clicked link
-      this.classList.add('active');
-    });
-  });
+//       // Add the 'active' class to the clicked link
+//       this.classList.add('active');
+//     });
+//   });
 
 // Nav Link Colors
 
-const navLinks = document.querySelectorAll('.nav-link');
-navLinks.forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault(); // Prevent the default link behavior
-    console.log(navLinks)
-    // Remove the 'active' class from all links
-    navLinks.forEach(link => link.classList.remove('active'));
+// const navLinks = document.querySelectorAll('.nav-link');
+// navLinks.forEach(link => {
+//   link.addEventListener('click', function(e) {
+//     // e.preventDefault(); // Prevent the default link behavior
+//     console.log(navLinks)
+//     // Remove the 'active' class from all links
+//     navLinks.forEach(link => link.classList.remove('active'));
 
-    // Add the 'active' class to the clicked link
-    this.classList.add('active');
-  });
-});
+//     // Add the 'active' class to the clicked link
+//     this.classList.add('active');
+//   });
+// });
 
